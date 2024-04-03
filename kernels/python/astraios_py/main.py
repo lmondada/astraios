267f4c72352027c5e684907e12909a1262ebb178
Luca Mondada <luca@mondada.net>
2024-04-03 06:19:15 +0200

    feat: Add py-kernel docker

--- kernels/python/astraios_py/main.py
+++ kernels/python/astraios_py/main.py
@@ -16,7 +16,7 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_CompilationServicer_to_server(CompilationServicer(), server)
     add_WorkerCreationServicer_to_server(WorkerCreationServicer(), server)
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port("[::]:9090")
     server.start()
     server.wait_for_termination()
 
